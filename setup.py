2ab263d6406b828dee88acb2d203ea2f06ed6d5b
Sebastian <ascacere92@gmail.com>
2019-10-24 15:55:09 -0500

    Updated README

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="calendario_bananero",
-    version="1.0.2",
+    version="1.0.3",
     author="Sebastian Caceres",
     author_email="chevass@protonmail.com",
     description="",
